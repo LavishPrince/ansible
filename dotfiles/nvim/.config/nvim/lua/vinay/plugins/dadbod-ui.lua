b49523d85dd7232c18b05b0ddbed8246aa39d9c8
Vinay DG <vinay.dg@travancoreanalytics.com>
2024-09-03 10:07:50 +0530

    Added Some customizations for dadbod plugin

--- dotfiles/nvim/.config/nvim/lua/vinay/plugins/dadbod-ui.lua
+++ dotfiles/nvim/.config/nvim/lua/vinay/plugins/dadbod-ui.lua
@@ -12,6 +12,12 @@ return {
 	},
 	init = function()
 		-- Your DBUI configuration
-		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.db_ui_use_nerd_fonts = 1 -- Use nerd fonts if available
+		vim.g.db_ui_win_position = "right" -- Position the UI on the left side
+		vim.g.db_ui_show_database_icon = 1 -- Show database icons
+
+		-- Optional: Customize the UI layout further
+		vim.g.db_ui_force_echo_notifications = 1 -- Show notifications
+		vim.g.db_ui_winwidth = 20 -- Set the width of the left panel
 	end,
 }
