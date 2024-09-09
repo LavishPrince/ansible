640e46c179cc98aa1f84c5f0d3c430c10f6104ca
Vinay DG <vinay.dg@travancoreanalytics.com>
2024-09-09 12:46:07 +0530

    Added Obsidian and Markdown support

--- dotfiles/nvim/.config/nvim/lua/vinay/core/options.lua
+++ dotfiles/nvim/.config/nvim/lua/vinay/core/options.lua
@@ -4,6 +4,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.conceallevel = 1
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
