b02ed1323d8c01cbf9b1e9eb10f4b284c0efbd15
Vinay DG <vinay.dg@travancoreanalytics.com>
2024-09-24 23:46:37 +0530

    Added Supermaven plugin

--- dotfiles/nvim/.config/nvim/lua/vinay/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/vinay/plugins/telescope.lua
@@ -10,45 +10,27 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local transform_mod = require("telescope.actions.mt").transform_mod
+
+		local trouble = require("trouble")
+		local trouble_telescope = require("trouble.sources.telescope")
+
+		-- or create your custom action
+		local custom_actions = transform_mod({
+			open_trouble_qflist = function(prompt_bufnr)
+				trouble.toggle("quickfix")
+			end,
+		})
 
 		telescope.setup({
 			defaults = {
-				pickers = {
-					find_files = {
-						hidden = true,
-						find_command = {
-							"rg",
-							"--files",
-							"--hidden",
-							"-uu",
-							"--no-ignore",
-							"--glob=!**/.git/*",
-							"--glob=!**/.idea/*",
-							"--glob=!**/.vscode/*",
-							"--glob=!**/build/*",
-							"--glob=!**/dist/*",
-							"--glob=!**/yarn.lock",
-							"--glob=!**/package-lock.json",
-						},
-					},
-				},
-				vimgrep_arguments = {
-					"rg",
-					"--color=never",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-					"--hidden", -- This flag includes hidden files
-					"--glob=!.git/", -- Exclude .git directory
-				},
 				path_display = { "smart" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+						["<C-t>"] = trouble_telescope.open,
 					},
 				},
 			},
@@ -59,7 +41,7 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
