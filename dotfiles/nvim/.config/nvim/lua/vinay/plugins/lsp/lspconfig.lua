924e1ef18ce3e58ad85593297372a4e9c92e55d2
Vinay DG <vinaydg2001@gmail.com>
2024-12-08 21:37:35 +0530

    added hover definition in vim

--- dotfiles/nvim/.config/nvim/lua/vinay/plugins/lsp/lspconfig.lua
+++ dotfiles/nvim/.config/nvim/lua/vinay/plugins/lsp/lspconfig.lua
@@ -26,11 +26,20 @@ return {
 				local opts = { buffer = ev.buf, silent = true }
 
 				-- set keybinds
+				vim.opt.updatetime = 500
+				vim.api.nvim_create_autocmd("CursorHold", {
+					callback = function()
+						vim.lsp.buf.hover()
+					end,
+				})
 				opts.desc = "Show LSP references"
 				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
 				opts.desc = "Go to declaration"
 				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+				opts.desc = "Go to definitions"
+				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- go to definitions
 
 				opts.desc = "Show LSP definitions"
 				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
