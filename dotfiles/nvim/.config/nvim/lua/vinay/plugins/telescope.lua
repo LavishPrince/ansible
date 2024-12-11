7f78a76d8329a96b3ca93e93cb61679f8a412261
Vinay DG <vinaydg2001@gmail.com>
2024-12-11 22:36:47 +0530

    changed the description for a keymap

--- dotfiles/nvim/.config/nvim/lua/vinay/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/vinay/plugins/telescope.lua
@@ -45,7 +45,7 @@ return {
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Find Git Files" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
