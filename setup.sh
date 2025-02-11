b4f1f6b9038b0032bf8f4bfad97abbb5cf492f72
Vinay DG <vinay.dg@travancoreanalytics.com>
2025-02-11 17:50:21 +0530

    add scripts to setup the machines

---
+++ setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+install_homebrew() {
+  echo "Installing Homebrew (macOS)..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Add to PATH (often handled by the installer, but good to include)
+  if [[ -f $HOME/.zshrc ]]; then
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shell)"' >>$HOME/.zshrc
+    source $HOME/.zshrc
+  elif [[ -f $HOME/.bashrc ]]; then
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shell)"' >>$HOME/.bashrc
+    source $HOME/.bashrc
+  fi
+
+  echo "Homebrew installed."
+}
+
+install_linuxbrew() {
+  echo "Installing Linuxbrew (Linux)..."
+  sudo apt-get update # Update package list
+  sudo apt-get install -y build-essential curl file git
+
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  if [[ -f $HOME/.zshrc ]]; then
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shell)"' >>$HOME/.zshrc
+    source $HOME/.zshrc
+  elif [[ -f $HOME/.bashrc ]]; then
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shell)"' >>$HOME/.bashrc
+    source $HOME/.bashrc
+  fi
+
+  echo "Linuxbrew installed."
+}
+
+# Function to install Nix (macOS)
+install_nix_macos() {
+  echo "Installing Nix (macOS)..."
+  sh <(curl -L https://nixos.org/nix/install)
+  echo "Nix installed. You may need to open a new terminal or source your shell configuration file (e.g., ~/.zshrc, ~/.bashrc) for the changes to take effect."
+}
+
+# Function to install Nix (Ubuntu/Linux)
+install_nix_linux() {
+  echo "Installing Nix (Linux)..."
+  sh <(curl -L https://nixos.org/nix/install) --daemon
+  echo "Nix installed. You may need to open a new terminal or source your shell configuration file (e.g., ~/.zshrc, ~/.bashrc) for the changes to take effect."
+
+}
+# Check if the OS is macOS
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  echo "macOS detected. Running mac.sh"
+  install_homebrew
+  install_nix_macos
+  ./mac.sh
+elif [[ "$(uname -s)" == "Linux" ]]; then
+  echo "Linux detected. Running linux.sh"
+  install_homebrew
+  install_nix_linux
+  ./linux.sh
+else
+  echo "Unknown operating system detected."
+fi
+
+cd dotfiles
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm/
+stow --adopt -t $HOME home
+echo "Dotfiles stowed successfully!"
+
+cp ./sshkeys/* $HOME/.ssh/
+ansible-vault decrypt $HOME/.ssh/LavishPrince
+ansible-vault decrypt $HOME/.ssh/Vinaydg
